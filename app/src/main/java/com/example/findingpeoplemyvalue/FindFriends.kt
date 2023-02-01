46c1e986418c98439dd67a7baab0d9736cb75eb9
Halil Mert <halilmertdeveliii@gmail.com>
2023-02-01 19:31:56 +0300

    fetch commit

--- app/src/main/java/com/example/findingpeoplemyvalue/FindFriends.kt
+++ app/src/main/java/com/example/findingpeoplemyvalue/FindFriends.kt
@@ -2,10 +2,26 @@ package com.example.findingpeoplemyvalue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_find_friends.*
 
 class FindFriends : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_friends)
+
+        val user = Firebase.auth.currentUser
+
+        if (user != null){
+            userNameTextView.text = user.email.toString()
+        }
+        else{
+            println("user is not here")
+        }
+
     }
+
 }
